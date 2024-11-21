9c695ee48e98d6157905e6891786d659df17da26
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-11-21 13:40:41 +0100

    Update use-okx-ui.hook.tsx

--- hooks/use-okx-ui.hook.tsx
+++ hooks/use-okx-ui.hook.tsx
@@ -27,9 +27,36 @@ export default function useOKXUI() {
 		initUI();
 	}, []);
 
+	const addChain = async () => {
+		if (!ui) return;
+		try {
+			await ui.provider.request({
+				method: 'wallet_addEthereumChain',
+				params: [
+					{
+						chainId: '747',
+						chainName: 'Flow',
+						nativeCurrency: {
+							name: 'Flow',
+							symbol: 'FLOW',
+							decimals: 18,
+						},
+						rpcUrls: ['https://mainnet.evm.nodes.onflow.org'],
+						blockExplorerUrls: ['https://evm.flowscan.io/'], // Optional
+					},
+				],
+			});
+			console.log('Chain added successfully.');
+		} catch (error) {
+			console.error('Failed to add chain:', error);
+		}
+	};
+
 	const openModal = async () => {
 		if (!ui) return;
 		try {
+			// Add the chain before opening the modal
+			await addChain();
 			const session = await ui.openModal({
 				namespaces: {
 					eip155: {
