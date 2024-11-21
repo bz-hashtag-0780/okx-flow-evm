8ee5d448daf5fd117ef3d7ff8c5f843bb64ee3a6
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-11-21 17:23:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+'use client';
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-'use client';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef, useState } from 'react';
 import { OKXUniversalConnectUI, THEME } from '@okxconnect/ui';
